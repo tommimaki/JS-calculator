85391154c6b1ef82bde9f5fcc9fa1ca7bd31627c
tommibish <74586216+tommimaki@users.noreply.github.com>
2022-12-14 16:09:39 -0500

    working web calc

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-const display = 123445;
 const prevNum = document.getElementById("prevNum");
 const curNum = document.getElementById("curNum");
 let currentNumber = "";
+
+
 let operatorvalue;
 
 function updateDisplay(num) {
@@ -11,51 +12,66 @@ function updateDisplay(num) {
 
 const clearDisplay = function () {
   prevNum.textContent = "";
-  curNum.textContent = 0;
+  curNum.textContent = '';
   currentNumber = "";
+};
+
+const deleteLast = function () {
+
+  currentNumber = currentNumber.toString().slice(0, -1);
+  curNum.textContent = currentNumber;
+  
 };
 const clearCur = function () {
-  curNum.textContent = 0;
+  curNum.textContent = '';
   currentNumber = "";
 };
 
-//
-///calculate function but only with add for now function
-
 const setOperator = function (operatorValue) {
-  prevNum.textContent = curNum.textContent;
+  prevNum.textContent = curNum.textContent + " " + operatorValue;
   clearCur();
   operatorvalue = operatorValue;
 };
 
 const equals = function () {
+  
   let num1 = parseFloat(prevNum.textContent);
   let num2 = parseFloat(curNum.textContent);
+
+  console.log(num1,num2)
   prevNum.textContent = "";
 
+  if(isNaN(num1) || isNaN(num2)){
+   console.log('Nans, wont operate') }
+   else{
+  
   switch (operatorvalue) {
     case "+":
-      curNum.textContent = add(num1, num2);
+      currentNumber =  add(num1, num2)
+      curNum.textContent = currentNumber ;
+     
       break;
     case "-":
       curNum.textContent = substract(num1, num2);
       break;
     case "*":
       curNum.textContent = multiply(num1, num2);
+      break;
     case "/":
+
       curNum.textContent = divide(num1, num2);
       break;
     default:
       console.log("error");
   }
+}
+
 };
 
-// operators
-
+// operator functions
 const add = function (num1, num2) {
   return num1 + num2;
 };
-
 const substract = function (num1, num2) {
   return num1 - num2;
 };
@@ -63,18 +79,25 @@ const multiply = function (num1, num2) {
   return num1 * num2;
 };
 const divide = function (num1, num2) {
+  if( num1 != 0 && num2 != 0){
   return num1 / num2;
+  }else {
+    prevNum.textContent = ''
+    curNum.textContent = '';
+    currentNumber = "";
+  return '';
+  }
 };
 
-// consts for buttongroups
+// consts for buttons and buttongroups
 
 const operators = document.querySelectorAll(".operator");
 const numbers = document.querySelectorAll(".numberButton");
 const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#del");
 const equalsButton = document.querySelector("#equals");
 
-// tie eventlisteners to buttons
-
+// eventlisteners to operators and numbers
 clearButton.addEventListener("click", () => {
   clearDisplay();
 });
@@ -82,21 +105,19 @@ clearButton.addEventListener("click", () => {
 equalsButton.addEventListener("click", () => {
   equals();
 });
+deleteButton.addEventListener("click", () => {
+  deleteLast(curNum);
+});
 
 operators.forEach((button) => {
   button.addEventListener("click", () => {
     let operatorVal = button.innerHTML;
     setOperator(operatorVal);
-    // calculate(calculate(prevNum, curNum));
-    //   calculator.chooseOperation(button.innerText)
-    //   calculator.updateDisplay()
   });
 });
 
 numbers.forEach((button) => {
   button.addEventListener("click", () => {
     updateDisplay(button.innerHTML);
-    //   calculator.chooseOperation(button.innerText)
-    //   calculator.updateDisplay()
   });
 });
